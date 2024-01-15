983906347477ee4111391ab425f662935dbfee38
vikashd03 <vikashdhanabal003@gmail.com>
2024-01-16 02:23:58 +0530

    added logs

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -14,6 +14,7 @@ export async function GET(request: NextRequest) {
       { status: 200 }
     );
   } catch (error: any) {
+    console.log(error.message);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
