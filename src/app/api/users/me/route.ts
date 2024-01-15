7b166d624ab174961b49dc8915ca4d9cdec8a3e7
vikashd03 <vikashdhanabal003@gmail.com>
2024-01-16 02:23:15 +0530

    first commit update

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -14,6 +14,6 @@ export async function GET(request: NextRequest) {
       { status: 200 }
     );
   } catch (error: any) {
-    NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
